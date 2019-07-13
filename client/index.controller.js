89c53ca4693fb7e0e869a76f7b6589264266a1b4
Amal Muslim <muslim.amal@yandex.ru>
2019-07-13 20:29:12 +0300

    add modal window

--- client/index.controller.js
+++ client/index.controller.js
@@ -3,9 +3,21 @@ import config from '../config'
 
 angular
   .module('myApp', [])
-  .controller('myController', ['$http', function($http) {
+  .controller('myController', ['$http', '$document', '$element', function($http, $document, $element) {
+
   const vm = this;
   const api = config.host;
+  const modal = angular.element(document.querySelector("#myModal"));
+
+  vm.openModal = function () {
+    // console.log(modal[0].style);
+    modal[0].style.display = "block";
+  }
+
+  vm.closeModal = function () {
+    modal[0].style.display = "none";
+  }
+
 
   // $http.get(api + '/diary').then(function(response) {
   //   console.log(response.data);
